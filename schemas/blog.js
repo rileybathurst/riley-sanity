c36c4dfa27192c5a3f76067e732000e220448da9
Riley <riley@rileybathurst.com>
2023-05-10 17:37:34 -0700

    blog

---
+++ schemas/blog.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'blog',
+  title: 'Blog',
+  type: 'document',
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+      description: 'Make it catchy',
+    },
+  ]
+}
